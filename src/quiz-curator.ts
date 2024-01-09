399c3555c89fa727060b6c4a6f4eca9d560e3a66
Damian <damiakpan@gmail.com>
2024-01-10 00:11:12 +0100

    Implemented quiz curator using openai api (#3)

---
+++ src/quiz-curator.ts
@@ -0,0 +1,55 @@
+import promptTemplate from "./promptTemplate";
+
+const APIKEY = import.meta.env.VITE_OPENAI_API_KEY as string;
+
+
+/**
+ * Compose a quiz from a given topic
+ */
+export const composeQuiz = async (topic: string): Promise<Quiz> => {
+  const api = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + APIKEY,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo-1106",
+      temperature: 0.9,
+      messages: [
+        {
+          "role": "system",
+          "content": promptTemplate,
+        },
+        {
+          "role": "user",
+          "content": `
+          Topic: ${topic}
+          `
+        },
+      ],
+    }),
+  });
+
+  const res = await api.json();
+  const data = res.choices[0].message.content.replace("```json", "").replace("```", "");
+  const questions = JSON.parse(data) as Question[];
+  return {
+    topic,
+    questions,
+  }
+}
+
+interface Question {
+  question: string;
+  answers: string[];
+
+  // Index of the correct answer
+  correctAnswer: number;
+}
+
+export interface Quiz {
+  topic: string;
+  questions: Question[];
+}
+
