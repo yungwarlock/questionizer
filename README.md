4b1b99f3e74aa684eb946fcb27e4a5b7fd4e3c1e
Damian Akpan <damiakpan@gmail.com>
2024-01-09 21:23:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Questionizer
+
+Convert anything into a quiz
+
+## Authors
+
+* Damian Akpan <damiakpan@gmail.com>
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex">
+      <h1>Questionizer</h1>
+    </div>
+  );
+}
+
+export default App
