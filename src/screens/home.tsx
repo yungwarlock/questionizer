2abc32c2be3ddcd1e46fc61f16ebfbdf321ae68f
Damian Akpan <damiakpan@gmail.com>
2024-01-17 13:07:10 +0100

    Updated home design to be responsive

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -32,29 +32,29 @@ const Home = ({quizId}: HomeProps): JSX.Element => {
   };
 
   const LeftSection = (
-    <div className="flex-grow flex flex-col gap-2 px-16 py-40 text-white">
-      <h3 className="text-7xl font-medium">Questionizer</h3>
-      <h6 className="text-2xl pl-2">Turn anything into a quiz</h6>
+    <div className="flex-grow z-20 md:pl-24 md:pt-40 flex flex-col items-center md:items-start justify-center md:justify-normal gap-2 text-white">
+      <h3 className="text-5xl md:text-7xl font-medium">Questionizer</h3>
+      <h6 className="text-xl pl-2">Turn anything into a quiz</h6>
     </div>
   );
 
   const RightSection = (
-    <div className="flex-grow flex flex-row-reverse p-16">
-      <img src={BackgroundImg} className="rounded-xl" />
+    <div className="fixed md:relative z-10 w-screen h-screen md:flex-grow flex flex-row-reverse md:p-20">
+      <img src={BackgroundImg} className="md:rounded-xl brightness-50 md:brightness-100 object-cover" />
     </div>
   );
 
   const Questionizer = (
-    <form onSubmit={onSubmit} className="absolute z-10 bottom-10 w-full h-20 px-16">
-      <div className="w-full relative h-20 flex items-center overflow-hidden rounded-full">
+    <form onSubmit={onSubmit} className="absolute z-20 bottom-6 md:bottom-8 w-full px-4 md:px-20">
+      <div className="w-full bg-white py-2 px-2 relative flex items-center overflow-hidden rounded-full">
         <input
-          className="flex-grow outline-none w-full placeholder-gray-600 h-full px-6 text-3xl rounded-l-full"
+          className="flex-grow outline-none w-full placeholder-gray-600 px-6 text-xl md:text-3xl rounded-l-full"
           placeholder="Topic"
           value={topic}
           onChange={(e) => setTopic(e.target.value)}
         />
-        <button type="submit" className="absolute hover:bg-secondaryLight right-4 bg-secondary p-3 rounded-full">
-          <ArrowRight className="w-10 h-10 m-auto" />
+        <button type="submit" className="hover:bg-secondaryLight right-4 bg-secondary p-1 md:p-3 rounded-full">
+          <ArrowRight className="w-10 h-10 m-auto text-white" />
         </button>
       </div>
     </form>
@@ -62,7 +62,7 @@ const Home = ({quizId}: HomeProps): JSX.Element => {
 
 
   return (
-    <div className="flex relative w-full h-full bg-background">
+    <div className="flex flex-col md:flex-row relative w-full h-full bg-background overflow-hidden">
       {loading && (
         <div className="fixed z-50 bg-black/60 w-full h-full flex flex-col gap-4 justify-center items-center">
           <HashLoader color="#A24527" size={70} />
