588f5b5d0272f12855b2dfc71a79e5600c038d5f
Damian Akpan <damiakpan@gmail.com>
2024-01-18 03:17:33 +0100

    Updated complete screen to be responsive

--- src/screens/complete.tsx
+++ src/screens/complete.tsx
@@ -3,35 +3,33 @@ import React from "react";
 
 const Complete = (): JSX.Element => {
   return (
-    <div className="w-screen h-screen bg-background flex flex-col gap-8 justify-center items-center">
-      <div className="flex flex-col items-center gap-2">
-        <h3 className="text-6xl font-medium text-white">Test Complete</h3>
-        <h3 className="text-2xl font-medium text-white">Achievements</h3>
-      </div>
-
-      <div className="flex justify-center items-center gap-28 py-16 bg-red-400 w-[50%]">
-        <div className="flex flex-col justify-center items-center">
-          <h3 className="text-6xl">4</h3>
-          <h3 className="text-lg">Days</h3>
-          <h3 className="text-md">Streak</h3>
+    <div className="w-screen h-screen bg-background flex justify-center items-center p-4">
+      <div className="py-8 px-4 md:p-12 w-full md:w-fit rounded-2xl bg-secondary flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center gap-2">
+          <h3 className="text-4xl md:text-6xl font-medium text-white">Test Complete</h3>
+          <h3 className="text-xl md:text-2xl font-medium text-white">Achievements</h3>
         </div>
-        <div className="flex flex-col justify-center items-center">
-          <h3 className="text-6xl">15</h3>
-          <h3 className="text-lg">Questions</h3>
-          <h3 className="text-md">Correct</h3>
+
+        <div className="flex justify-between items-center text-white py-16 px-4 md:px-2 w-full">
+          <div className="flex flex-col justify-center items-center">
+            <h3 className="text-4xl md:text-6xl font-bold">4</h3>
+            <h3 className="text-xs md:text-lg">Days Streak</h3>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <h3 className="text-4xl md:text-6xl font-bold">15</h3>
+            <h3 className="text-xs md:text-lg">Questions Correct</h3>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <h3 className="text-4xl md:text-6xl font-bold">3</h3>
+            <h3 className="text-xs md:text-lg">mins time</h3>
+          </div>
         </div>
-        <div className="flex flex-col justify-center items-center">
-          <h3 className="text-6xl">3</h3>
-          <h3 className="text-lg">mins</h3>
-          <h3 className="text-md">time</h3>
+
+        <div className="w-full md:px-4 flex justify-between items-center">
+          <button className="px-6 py-3 rounded-2xl bg-secondaryLight hover:bg-secondaryLight">Try again</button>
+          <button className="px-6 py-3 rounded-2xl bg-secondaryLight hover:bg-secondaryLight">Corrections</button>
         </div>
       </div>
-
-      <div className="flex justify-center items-center gap-24 w-[50%]">
-        <button className="px-8 py-4 rounded-2xl bg-red-400 hover:bg-red-300">Try again</button>
-        <button className="px-8 py-4 rounded-2xl bg-red-400 hover:bg-red-300">Corrections</button>
-      </div>
-
     </div>
   );
 };
